48b9268ac00f5995fd9eb58be2e581fe75139d9d
Irena Buchwald <irena.buchwald@gmail.com>
2019-11-08 14:07:33 +0100

    test S-001 Zamowienia klientow zagranicznych potwierdza dobre dzialanie aplikacji

--- scenariusze.sql
+++ scenariusze.sql
@@ -1,5 +1,5 @@
 -- S-001 Zamówienia klientow zagranicznych, klient  S-001-P01 - adres w Niemczech | Helga Muller| Lutzowplatz 17, 10785 Berlin, Germany| skuter elektryczny bialy, 1 szt.|
- 
+
 insert into klienci (nazwa, kraj, kod_pocztowy, miasto, ulica, numer) values ('Helga Muller', 'Niemcy', '10785', 'Berlin', 'Lutzowplatz', '17');
 set @last_klient_id = last_insert_id();
 
@@ -22,9 +22,15 @@ set @last_klient_id = last_insert_id();
 iNSERT INTO zamowienia(id_klienta) values (@last_klient_id);
 set @last_zamowienie_id = last_insert_id();
 
+
+
 INSERT INTO szczegoly(id_zamowienia, LP, id_produktu, ilosc) values (@last_zamowienie_id, 1, @ziel_ro_bieg, 1);
 INSERT INTO szczegoly(id_zamowienia, LP, id_produktu, ilosc) values (@last_zamowienie_id, 2, @czer_ro_bieg, 1);
 
 insert into statusy_zamowien (id_zamowienia, data, status) values (@last_zamowienie_id,'2019-01-05 08:13:20','zlozone');
 insert into statusy_zamowien (id_zamowienia, data, status) values (@last_zamowienie_id,'2019-01-06 10:14:45','oplacone');
 insert into statusy_zamowien (id_zamowienia, data, status) values (@last_zamowienie_id,'2020-01-09 21:23:25','zrealizowane');
+
+
+select *from klienci
+where kraj!= 'Polska';
