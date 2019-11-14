004b4fc9e57e7d52ad9359db8d92628d4959b8c6
Irena Buchwald <irena.buchwald@gmail.com>
2019-11-14 14:22:41 +0100

    Prace nad kwerenda do scenariusza S-002

--- scenariusze.sql
+++ scenariusze.sql
@@ -24,6 +24,7 @@ delete from zamowienia where id_klienta in (select id_klienta from klienci
 /* Na koniec usuwamy klientow pasujacych do wzorca */
 delete from klienci where nazwa like @identyfikator_scenariusza;
 
+
 /* Scenariusz S-001-P01 tworzenie danych */
 insert into klienci (nazwa, kraj, kod_pocztowy, miasto, ulica, numer) values ('Klient S-001-P01', 'Niemcy', 'D-1215', 'Berlin', 'Alexanderplatz', '12');
 set @last_klient_id = last_insert_id();
@@ -51,12 +52,41 @@ insert into statusy_zamowien (id_zamowienia, data, status) values (@last_zamowie
 /* Zapytania zdefiniowane w scenariuszach testowych */
 
 /* Scenariusz S-001 - kwerenda wybierająca zamówienia klientów zagranicznych */
-select klienci.nazwa, zamowienia.id_zamowienia, szczegoly_zamowien.LP, szczegoly_zamowien.nazwa_produktu 
+/* select klienci.nazwa, zamowienia.id_zamowienia, szczegoly_zamowien.LP, szczegoly_zamowien.nazwa_produktu 
   from klienci, zamowienia, szczegoly_zamowien
  where zamowienia.id_klienta = klienci.id_klienta
    and szczegoly_zamowien.id_zamowienia = zamowienia.id_zamowienia
-   /* tu jest sedno zapytania */
-   and klienci.kraj <> 'Polska';
+ 
+   and klienci.kraj <> 'Polska';*/
+
+/* ------------------------------------------------------------------------------------------------------------------------------*/
+
+/* Pobranie wspólnych zmiennych */
+set @ziel_ro_bieg = (select id_produktu from produkty where nazwa = 'zielony rowerek biegowy');
+set @hul_ele = (select id_produktu from produkty where nazwa = 'hulajnoga elektryczna');
+
+
+
+/* Czyszczenie wszystkich danych testowych */
+/* Ten identyfikator pasuje do kazdegow wzorca Klient S-001-P01, wiec usuniemy wszystkie dane wszystkich scenariuszy */
+set @identyfikator_scenariusza = 'Klient S-___-___' COLLATE utf8mb4_unicode_ci;
+
+/* Usuwamy statusy zamowien zlozonych przez klientow pasujacych do wzorca */
+delete from statusy_zamowien where id_zamowienia in (select id_zamowienia from zamowienia, klienci 
+                                                     where zamowienia.id_klienta = klienci.id_klienta 
+                                                       and klienci.nazwa like @identyfikator_scenariusza);
+
+/* Usuwamy szczegoly zamowien zlozonych przez klientow pasujacych do wzorca */
+delete from szczegoly where id_zamowienia in (select id_zamowienia from zamowienia, klienci 
+                                              where zamowienia.id_klienta = klienci.id_klienta 
+                                              and klienci.nazwa like @identyfikator_scenariusza);
+
+/* Usuwamy zamowienia zlozone przez klientow pasujacych do wzorca */
+delete from zamowienia where id_klienta in (select id_klienta from klienci 
+                                            where nazwa like @identyfikator_scenariusza);
+
+/* Na koniec usuwamy klientow pasujacych do wzorca */
+delete from klienci where nazwa like @identyfikator_scenariusza;
 
 
 
@@ -113,4 +143,9 @@ INSERT INTO szczegoly(id_zamowienia, LP, id_produktu, ilosc) values (@last_zamow
 insert into statusy_zamowien (id_zamowienia, data, status) values (@last_zamowienie_id,'2019-02-11 12:13:15','zlozone');
 
 
-select * from klienci;
+select klienci.nazwa, zamowienia.id_zamowienia, szczegoly_zamowien.LP, szczegoly_zamowien.nazwa_produktu, statusy_zamowien.status
+  from klienci, zamowienia, szczegoly_zamowien, statusy_zamowien
+ where zamowienia.id_klienta = klienci.id_klienta
+   and szczegoly_zamowien.id_zamowienia = zamowienia.id_zamowienia
+  and status = 'oplacone';
+
